adaeb32694ce1bca146784da97702d54aa8ca38f
med15060 <34769155+med15060@users.noreply.github.com>
2017-12-22 11:05:25 +0000

    Update dow-csv.py

--- dow-csv.py
+++ dow-csv.py
@@ -1,5 +1,6 @@
 from urllib.request import urlretrieve
-for x in range(5):
-    url = 'http://localhost:8888/?format=csv&q=[twitter:maroc]&pageno='+str(x) 
-    dst = 'omacsvfile'+str(x)+'.csv'
-    urlretrieve(url, dst)
+try:
+   for x in range(5):
+       url = 'http://localhost:8888/?format=csv&q=[twitter:maroc]&pageno='+str(x) 
+       dst = 'omacsvfile'+str(x)+'.csv'
+       urlretrieve(url, dst)
